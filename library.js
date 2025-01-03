d011a488148af52ee1c5ea6a0e3964a31862edb2
Jakub Rutrle <rutrle@seznam.cz>
2025-01-03 02:31:06 +0100

    adding new book functional

--- library.js
+++ library.js
@@ -1,3 +1,16 @@
+var dialog = document.querySelector("dialog");
+var showButton = document.querySelector("dialog + button");
+var closeButton = document.querySelector("dialog button");
+var submitNewBook = document.querySelector("#new-submit");
+var formNewBook = document.querySelector("#new-book-form");
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", function () {
+    dialog.showModal();
+});
+// "Close" button closes the dialog
+closeButton.addEventListener("click", function () {
+    dialog.close();
+});
 var Book = /** @class */ (function () {
     function Book(title, author, pages_number) {
         this.title = title;
@@ -18,11 +31,17 @@ function showLibrary() {
         var bookRow = document.createElement("p");
         bookRow.textContent = "".concat(element.title, " by ").concat(element.author, " long ").concat(element.pages_number);
         var libraryDiv = document.querySelector("#library-div");
-        console.log(libraryDiv);
         libraryDiv.appendChild(bookRow);
     }
     ;
 }
+formNewBook.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var form = event.target;
+    var formData = new FormData(form);
+    addBookToLibrary(formData.get("title"), formData.get("author"), formData.get("pages_number"));
+    showLibrary();
+});
 addBookToLibrary("Hobit", "Tolkien", 123);
 addBookToLibrary("Harry potter", "Rowling", 321);
 addBookToLibrary("Dance of Dragons", "GMR", 456);
--- library.ts
+++ library.ts
@@ -1,3 +1,22 @@
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+const submitNewBook = document.querySelector("#new-submit")
+const formNewBook = document.querySelector("#new-book-form")
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+
+
+
 class Book{
     title: String;
     author: String;
@@ -24,11 +43,20 @@ function showLibrary(){
     const bookRow = document.createElement("p");
     bookRow.textContent =  `${element.title} by ${element.author} long ${element.pages_number}`
     const libraryDiv = document.querySelector("#library-div")
-    console.log(libraryDiv)
     libraryDiv.appendChild(bookRow)
 
     };
 }
+
+formNewBook.addEventListener("submit", function(event){
+    event.preventDefault()
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    addBookToLibrary(formData.get("title"),formData.get("author"),formData.get("pages_number"));
+    showLibrary()
+
+
+})
 
 addBookToLibrary("Hobit", "Tolkien", 123);
 addBookToLibrary("Harry potter", "Rowling", 321);
@@ -37,3 +65,8 @@ addBookToLibrary("Witcher", "Sapkowski", 654);
 addBookToLibrary("Narnie", "Lewis", 789);
 
 showLibrary()
+
+
+
+
+
